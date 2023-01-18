76af9e6fcd7498b5ebfc2d68ce01fc345b199955
Usman Butt <40109604+UsmanAliButt@users.noreply.github.com>
2023-01-18 19:39:54 +0100

    Propely checked on physical hardware

--- app/src/main.c
+++ app/src/main.c
@@ -5,28 +5,26 @@ Dated : 6-october-2022
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
-//#include <../Toggle_led_driver/Toggledriver.h>
+#include <toggledriver.h>
 
-#define LED0_NODE DT_ALIAS(led0)
-static const struct device *dev = DEVICE_DT_GET(DT_ALIAS(led0));
-static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
+/*
+Get device from node label. See toggle.overlay file. Compiled
+files contains full device tree.
+*/
+static const struct device *dev = DEVICE_DT_GET(DT_NODELABEL(drivertoggle));
 
-void main(void){
+void main(void){	
 	
-	if (!device_is_ready(dev)) {
-		//printk("Error: GPIO device %s is not ready\n",toggle_led->name);
-		//return;
-	}
-	printk("device is %p, name is %s\n", dev, dev->name);
-
-	if (!device_is_ready(led.port)) {
-		//printk("Error: GPIO device %s is not ready\n",toggle_led->name);
-		//return;
+	if (!device_is_ready(dev)) {		//Check if device is created and ready
+		printk("Driver not ready");
+		return;
 	}
 	
+	//Print device name and object instance address
+	printk("Device is %p, name is %s\n", dev, dev->name);
+		
 	while(1){
-		k_sleep(K_MSEC(1000));
-		}
-	
+		alter(500);						//Alter led after ever 0.5 seconds 
+	}
 	
 }
